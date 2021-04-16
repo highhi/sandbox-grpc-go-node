11ef87dbcdb2a941b5441eb0e9aaf4b983631102
highhi <kazu8160@gmail.com>
2021-04-16 21:24:20 +0900

    fix migration file for users

--- app/migration/000001_create_users_table.up.sql
+++ app/migration/000001_create_users_table.up.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS users(
-   id serial PRIMARY KEY,
-   username VARCHAR (50) NOT NULL,
-   password VARCHAR (50) NOT NULL,
-   email VARCHAR (300) UNIQUE NOT NULL
+   id VARCHAR (300) PRIMARY KEY NOT NULL,
+   name VARCHAR (50) NOT NULL,
+   email VARCHAR (300) UNIQUE NOT NULL,
+   created_at timestamp NOT NULL,
+   updated_at timestamp NOT NULL
 );
 
