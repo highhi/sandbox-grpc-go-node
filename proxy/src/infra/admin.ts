c6045c6523743dd6a8bb0c5c01b62155b96d9a5c
highhi <kazu8160@gmail.com>
2021-04-23 22:15:08 +0900

    Manage credentials with direnv

--- proxy/src/infra/admin.ts
+++ proxy/src/infra/admin.ts
@@ -1,12 +1,16 @@
 import * as admin from 'firebase-admin';
-import serviceAccount from '../config/service-account.json';
 
 export const getAdmin = (): admin.app.App => {
   if (admin.apps.length > 0) {
     return admin.apps[0] as admin.app.App;
   } else {
     const app = admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount as any),
+      credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+        // see: https://stackoverflow.com/questions/50299329/node-js-firebase-service-account-private-key-wont-parse
+        privateKey: (process.env.FIREBASE_ADMIN_PRIVATE_KEY as string).replace(/\\n/g, "\n"),
+      }),
       databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
     });
     return app;
